80d233c441165d9bdca9b1602d3528a70babeb4d
Sacha Moira Rinn <Moira.Rinn@gmail.com>
2021-11-21 23:45:49 -0500

    Update. Need to refactor most recent algo.

--- scratch_pad.js
+++ scratch_pad.js
@@ -1,3 +1,5 @@
+//CodeWars
+
 // Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
 // Examples
 
@@ -27,6 +29,10 @@ const toCapital = (phrase) => {
 };
 
 console.log(toCapital(phrase));
+
+//HackerRank
+
+//Grading Students
 
 const rounding = (grades) => {
 	if (grades > 38) {
@@ -68,6 +74,8 @@ const rounding = (grades) => {
 };
 console.log(rounding([73, 67, 38, 33]));
 
+//Apple and Orange
+
 const fruits = (s, t, a, b, arr1, arr2) => {
 	let appleCnt = 0;
 	let orangeCnt = 0;
@@ -86,16 +94,64 @@ const fruits = (s, t, a, b, arr1, arr2) => {
 fruits(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
 // console.log(fruits(7,11,5,15, [-2, 2, 1], [5, -6]));
 
+//Number Line Jumps
+
 const jump = (x1, v1, x2, v2) => {
 	let k1 = x1 + v1;
 	let k2 = x2 + v2;
-	for (let i = 0; i < 10000; i++) {
+	for (let i = 0; i < 10010; i++) {
 		k1 += v1;
 		k2 += v2;
 		if (k1 === k2) {
 			return 'yes';
-		} else {
-			return 'no';
 		}
 	}
+	return 'no';
 };
+
+console.log(jump(0, 3, 4, 2));
+
+//Between Two Sets
+
+let arr1 = [3, 4];
+let arr2 = [24, 48];
+
+const getTotalX = (a, b) => {
+	let arr3 = [];
+
+	a.sort((a, b) => b - a);
+	b.sort((a, b) => b - a);
+
+	for (let i = 1; i < b[0]; i++) {
+		b.map((itm) => itm % i === 0 && arr3.push(i));
+	}
+	// let arr4 = arr3.filter((itm) => itm >= a[0]);
+	let arr5 = [];
+	arr3.filter((itm) => arr5.indexOf(itm) === -1 && arr5.push(itm));
+
+	b.map((val) => {
+		let i = 0;
+		while (i < arr5.length) {
+			val % arr5[i] !== 0 ? arr5.splice(i, 1) : i++;
+		}
+		// arr5.map((itm, idx) => val % itm !== 0 && arr5.splice(idx, 1));
+	});
+
+	console.log('5', arr5);
+	a.map((val) => {
+		let i = 0;
+		while (i < arr5.length) {
+			arr5[i] % val !== 0 ? arr5.splice(i, 1) : i++;
+		}
+	});
+
+	return arr5.length;
+};
+console.log(getTotalX(arr1, arr2));
+
+a = [3, 4];
+arr5 = [8, 12, 24];
+a.map((val) => {
+	arr5.map((itm, idx) => itm % val !== 0 && arr5.splice(idx, 1));
+});
+console.log(arr5);
